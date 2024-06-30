65ac6ec4843bb1180c1b5697673fe90b85115fa2
Jatin <csecec.1702674@gmail.com>
2024-06-30 21:39:05 +0530

    add updated code - try 2

--- src/main/java/org/dashboard/dashboardjavaspringmongodb/api/repository/UserRepository.java
+++ src/main/java/org/dashboard/dashboardjavaspringmongodb/api/repository/UserRepository.java
@@ -2,6 +2,11 @@ package org.dashboard.dashboardjavaspringmongodb.api.repository;
 
 import org.dashboard.dashboardjavaspringmongodb.api.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmailAndUsername(String email, String username);
 }
